28683b003f10f22308827b453cd65047d20cf3a6
Carlos Daniel Granados Orduna <carlos.daniel.g.orduna@gmail.com>
2025-06-19 20:58:24 -0600

    Update JobController.java
    Se agregaron los HttpStatus

--- src/main/java/com/CarlosDGO/firstjobapp/job/JobController.java
+++ src/main/java/com/CarlosDGO/firstjobapp/job/JobController.java
@@ -1,12 +1,11 @@
 package com.CarlosDGO.firstjobapp.job;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class JobController {
@@ -17,13 +16,21 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+        return ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public  ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<>("Job added succ",HttpStatus.CREATED);
+    }
+    @GetMapping("/jobs/{id}")
+    public ResponseEntity<Job> getJobByID(@PathVariable Long id){
+        Job job=jobService.getJobById(id);
+        if (job != null)
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
+
