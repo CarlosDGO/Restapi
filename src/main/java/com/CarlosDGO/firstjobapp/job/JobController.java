fee2d1818296c0d5b324c455716b34e90591439f
Carlos Daniel Granados Orduna <carlos.daniel.g.orduna@gmail.com>
2025-06-26 18:50:22 -0600

    Update JobController.java

--- src/main/java/com/CarlosDGO/firstjobapp/job/JobController.java
+++ src/main/java/com/CarlosDGO/firstjobapp/job/JobController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
     private JobService jobService;
 
@@ -15,31 +16,31 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping("/jobs")
+    @GetMapping
     public ResponseEntity<List<Job>> findAll(){
         return ResponseEntity.ok(jobService.findAll());
     }
 
-    @PostMapping("/jobs")
+    @PostMapping
     public  ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
         return new ResponseEntity<>("Job added succ",HttpStatus.CREATED);
     }
-    @GetMapping("/jobs/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Job> getJobByID(@PathVariable Long id){
         Job job=jobService.getJobById(id);
         if (job != null)
             return new ResponseEntity<>(job, HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @DeleteMapping("/jobs/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteJob(@PathVariable Long id){
         boolean deleted = jobService.deleteJobById(id);
         if(deleted)
             return new ResponseEntity<>("Job Deleted succ", HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-  @PutMapping("/jobs/{id}")
+  @PutMapping("/{id}")
     public ResponseEntity<String> updateJob(@PathVariable Long id,
                                             @RequestBody Job updatedJob){
         boolean updated = jobService.updateJob(id, updatedJob);
