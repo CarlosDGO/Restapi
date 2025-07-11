88e34759dbf5f73ea9f147490fc140db76d06f9c
Carlos Daniel Granados Orduna <carlos.daniel.g.orduna@gmail.com>
2025-07-10 22:09:40 -0600

    Update CompanyRepository.java

--- src/main/java/com/CarlosDGO/firstjobapp/Company/CompanyRepository.java
+++ src/main/java/com/CarlosDGO/firstjobapp/Company/CompanyRepository.java
@@ -1,5 +1,7 @@
 package com.CarlosDGO.firstjobapp.Company;
 
-public interface CompanyRepository {
-  
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+
 }
